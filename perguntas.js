e3685ef4fef8f7f25dd2272e81e8310aafab6d90
MateusJRJunior <168046117+MateusJRJunior@users.noreply.github.com>
2024-11-04 19:44:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'O Monte Everest tem 8.848 metros de altitude e localiza-se no Nepal, um país asiático que faz fronteira com a China e com a Índia.'
+)
+
+criaCartao(
+    'Aluno',
+    '3ºD',
+    'Mateus José Ribeiro Júnior'
+)
+
+criaCartao(
+    'Geografia',
+    'Que país tem o formato de uma bota?',
+    'No mapa da Europa, a Itália se destaca por parecer uma bota de cano e salto altos.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz BOA SORTE em Inglês?',
+    'BOA SORTE em ingles é Good Luck (gud lãqui)'
+)
